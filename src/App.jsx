680db312b74033f79607468d058efd6e75aaa394
Henry Liang <henry.liang@tidepool.org>
2025-12-22 21:34:52 -0800

    add lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react'
 import './App.css'
 
 const URL = 'https://script.google.com/macros/s/AKfycbz3aFm1CoXUI8m9w-pX1r_28uwdqXcS2eFDCnbra4EPKs_WuTVi3UZWyzviJFPgr_sm/exec?path=MAIN';
+
+const Image = ({ item }) => {
+  const sectionRef = useRef(null);
+  const [isRendered, setIsRendered] = useState(false);
+
+  const onIntersect = useCallback((entries) => {
+    if (entries[0].isIntersecting) setIsRendered(true);
+  }, []);
+
+  useEffect(() => {
+    if (sectionRef?.current) {
+      const observer = new IntersectionObserver(onIntersect)
+      observer.observe(sectionRef.current)
+
+      return () => observer.disconnect();
+    }
+  }, [sectionRef, onIntersect]);
+
+
+  return (
+    <section style={{ minHeight: '240px', border: '1px solid #ccc' }}>
+      {isRendered ? (
+        <img src={`${item.Folder}${item.Filename}`} width='240px' />
+      ) : null}
+
+      <div ref={sectionRef}></div>
+    </section>
+  )
+}
 
 function App() {
   const [data, setData] = useState([]);
@@ -10,7 +39,7 @@ function App() {
     fetch(URL)
       .then(res => res.json())
       .then(data => setData(data?.data || []));
-  })
+  }, [])
 
   if (!data.length) return 'Loading ...';
 
@@ -18,13 +47,9 @@ function App() {
     <section>
       <h1>Counterfeit Currency & Document Index</h1>
 
-      <section style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr'}}>
+      <section style={{ display: 'grid', gridTemplateColumns: '1fr'}}>
         {data.map((item, index) => {
-          return (
-            <div key={index} style={{ width: '100%' }}>
-              <img src={`${item.Folder}${item.Filename}`} width='240px' />
-            </div>
-          )
+          return <Image key={index} item={item} />
         })}
       </section>
     </section>
